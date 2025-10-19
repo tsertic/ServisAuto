d2397be1af1aba747f190e19eed0da167a3da635
Tomislav <tsertic5@gmail.com>
2025-10-19 15:23:06 +0200

    Service entity for auto service offerings

---
+++ ServisAuto.domain/Entities/Service.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServisAuto.domain.Entities
+{
+    public class Service
+    {
+        //pk
+        public int ServiceId { get; set; }
+
+        //basic informations
+        public string Name { get; set; } = string.Empty;
+        public string? Descritpion { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int DurationMinutes { get; set; }
+
+        //status
+        public bool IsActive { get; set; } = true;
+
+        //timestamps
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        //navigation properties
+
+       // public ICollection<Reservation> Reservations { get; set; } = new List<Reservation>();
+        
+    }
+}
