1b0916ce749991c9b5c705fbf94127979d8c0c55
Tomislav <tsertic5@gmail.com>
2025-10-19 11:24:58 +0200

    Add enums for user roles and reservation status

---
+++ ServisAuto.domain/Enums/ReservationStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServisAuto.domain.Enums
+{
+    public enum ReservationStatus
+    {
+        Pending = 1,
+        Confirmed = 2,
+        Rejected = 3,
+        Completed = 4,
+        Cancelled = 5
+    }
+}
