cb466956f1709300af05f20340dc1a5a213c6f04
Tomislav <tsertic5@gmail.com>
2025-10-20 22:16:35 +0200

    ApplicationDbContext with DbSets for all entities

---
+++ ServisAuto.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ServisAuto.domain.Entities;
+
+namespace ServisAuto.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; } = null!;
+        public DbSet<Vehicle> Vehicles { get; set; }= null!;
+        public DbSet<Reservation> Reservations { get; set; }= null!;
+        public DbSet<Service> Services { get; set; }=null!;
+    }
+}
